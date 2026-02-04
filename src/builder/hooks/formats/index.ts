6c141f3e0aba91f45f734afbd103ec289ba9ef69
Aaron Wentt <945181+Proof757@users.noreply.github.com>
2026-02-04 02:47:53 -0500

    adding js and typescript formats

--- src/builder/hooks/formats/index.ts
+++ src/builder/hooks/formats/index.ts
@@ -1,11 +1,14 @@
 import { cssVariablesClassMode, cssVariablesMediaMode } from './css';
 import { debug } from './debug';
+import { javascriptUmdWithModes, typescriptDeclarations } from './js';
 import { scssVariablesClassMode, scssVariablesMediaMode } from './scss';
 
 export default [
     cssVariablesClassMode,
     cssVariablesMediaMode,
     debug,
+    javascriptUmdWithModes,
+    typescriptDeclarations,
     scssVariablesClassMode,
     scssVariablesMediaMode,
 ];

84bf16ba867212d5e13c2921a6f8c14409a1f530
Aaron Wentt <945181+Proof757@users.noreply.github.com>
2026-02-04 02:52:19 -0500

    adding android format

--- src/builder/hooks/formats/index.ts
+++ src/builder/hooks/formats/index.ts
@@ -1,3 +1,4 @@
+import { androidResourcesWithModes, androidResourcesLight, androidResourcesDark, androidKotlinTheme, androidDimens } from './android';
 import { cssVariablesClassMode, cssVariablesMediaMode } from './css';
 import { debug } from './debug';
 import { iosSwiftEnumWithModes, iosSwiftEnumWithModesLegacy } from './ios';
@@ -5,6 +6,11 @@ import { javascriptUmdWithModes, typescriptDeclarations } from './js';
 import { scssVariablesClassMode, scssVariablesMediaMode } from './scss';
 
 export default [
+    androidResourcesWithModes,
+    androidResourcesLight,
+    androidResourcesDark,
+    androidKotlinTheme,
+    androidDimens,
     cssVariablesClassMode,
     cssVariablesMediaMode,
     debug,

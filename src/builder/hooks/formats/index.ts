e4c8763cde0e4d68d076e4485c45578bc186530e
Aaron Wentt <945181+Proof757@users.noreply.github.com>
2026-02-04 02:41:44 -0500

    adding css and scss formats, which use genFormatter to generate format config

--- src/builder/hooks/formats/index.ts
+++ src/builder/hooks/formats/index.ts
@@ -1,4 +1,11 @@
+import { cssVariablesClassMode, cssVariablesMediaMode } from './css';
 import { debug } from './debug';
+import { scssVariablesClassMode, scssVariablesMediaMode } from './scss';
+
 export default [
+    cssVariablesClassMode,
+    cssVariablesMediaMode,
     debug,
+    scssVariablesClassMode,
+    scssVariablesMediaMode,
 ];

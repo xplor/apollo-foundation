4733e30bee7d7e85be0d18d8b2f61981673a5fbb
Aaron Wentt <945181+Proof757@users.noreply.github.com>
2026-02-04 02:52:19 -0500

    adding android format

--- src/builder/hooks/formats/index.ts
+++ src/builder/hooks/formats/index.ts
@@ -1,8 +1,20 @@
+import {
+    androidResourcesWithModes,
+    androidResourcesLight,
+    androidResourcesDark,
+    androidKotlinTheme,
+    androidDimens,
+} from './android';
 import { cssVariablesClassMode, cssVariablesMediaMode } from './css';
 import { debug } from './debug';
 import { scssVariablesClassMode, scssVariablesMediaMode } from './scss';
 
 export default [
+    androidResourcesWithModes,
+    androidResourcesLight,
+    androidResourcesDark,
+    androidKotlinTheme,
+    androidDimens,
     cssVariablesClassMode,
     cssVariablesMediaMode,
     debug,

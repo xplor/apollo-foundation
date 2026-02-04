049ac59b25b342deb4983c2a412fb14acc8dc098
Aaron Wentt <945181+Proof757@users.noreply.github.com>
2026-02-04 01:36:03 -0500

    adding new file structure and new global tokens
    deleting the legacy tokens once new build process is added

---
+++ src/tokens/shared/web/z-index.ts
@@ -0,0 +1,12 @@
+const zIndexArr = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+const zIndexes = zIndexArr.reduce((acc, zIndex) => ({
+    ...acc,
+    [`${zIndex}`]: {
+        value: zIndex,
+        type: 'other',
+    }
+}), {});
+
+export default {
+    'z-index': zIndexes,
+};

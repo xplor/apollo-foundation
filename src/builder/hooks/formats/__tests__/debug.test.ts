82913724c62af84722adc778e4ec5836bdb713fd
Aaron Wentt <945181+Proof757@users.noreply.github.com>
2026-02-23 14:22:52 -0500

    removing `@ts-expect-error` comments

--- src/builder/hooks/formats/__tests__/debug.test.ts
+++ src/builder/hooks/formats/__tests__/debug.test.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from 'vitest';
+import { makeTestDict } from 'src/builder/utils/make-test-dict';
 import { debug } from '../debug';
 
 describe('debug format', () => {
@@ -7,12 +8,35 @@ describe('debug format', () => {
     });
 
     it('outputs stringified dictionary.tokens', async () => {
+        const allTokens = [
+            {
+                path: ['color', 'background', 'primary'],
+                name: 'xplColorBackgroundPrimary',
+                value: '.init(red: 1, green: 1, blue: 1, alpha: 1)',
+                type: 'color',
+                original: { value: '#ffffff', key: 'xplColorBackgroundPrimary' },
+                attributes: { category: 'color' },
+                filePath: '',
+                isSource: true,
+            },
+            {
+                path: ['color', 'dark', 'background', 'primary'],
+                name: 'xplColorBackgroundPrimaryDark',
+                value: '.init(red: 0, green: 0, blue: 0, alpha: 1)',
+                type: 'color',
+                original: { value: '#000000', key: 'xplColorBackgroundPrimaryDark' },
+                attributes: { category: 'color' },
+                filePath: '',
+                isSource: true,
+            },
+        ];
         const tokens = { color: { background: { value: '#fff' } } };
+        const platform = { prefix: 'xpl' };
         const result = await debug.format!({
-            // @ts-expect-error: TS thinks this fixture doesn't meet reqs for formatFn but its fine
-            dictionary: { tokens, allTokens: [], unfilteredTokens: [] },
+            dictionary: makeTestDict(allTokens, tokens, { unfilteredTokens: [] }),
             file: {},
             options: {},
+            platform,
         }) as string;
         expect(() => JSON.parse(result)).not.toThrow();
         expect(JSON.parse(result)).toEqual(tokens);
